084ddfce28459a1581b84811de2e01cdda0ba218
Johan Meneses <johanmeci03@gmail.com>
2022-11-25 20:56:50 -0500

    Component structure and styles

---
+++ src/components/PokemonPicture.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="pokemon-container">
+    
+    <img 
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+      class="hidden-pokemon"
+      alt="pokemon">
+
+    <img 
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+      class="fade-in"
+      alt="pokemon">
+
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<!--scope -> solo aplica el estilo al componente-->
+<style scoped>
+
+  /* Pokemon Picture */
+  .pokemon-container {
+    height: 200px;
+    display: flex;
+    justify-content: center;
+  }
+
+  img {
+    height: 200px;
+    position: absolute;
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+  }
+
+  .hidden-pokemon {
+    filter: brightness(0);
+  }
+
+</style>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>¿Quien es este Pokémon?</h1>
+
+  <!--Picture-->
+  <PokemonPicture />
+
+  <!--Options-->
+  <PokemonOptions />
+
+</template>
+
+<script>
+import PokemonPicture from '@/components/PokemonPicture'
+import PokemonOptions from '@/components/PokemonOptions'
+
+export default {
+  components: {
+    PokemonPicture,
+    PokemonOptions
+  }
+}
+</script>
+
+<style>
+
+</style>
