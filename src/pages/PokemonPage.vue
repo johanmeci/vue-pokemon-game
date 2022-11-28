d44745c7094d6ef8c021a5e8f17d9ff67b3d38ba
Johan Meneses <johanmeci03@gmail.com>
2022-11-28 15:02:43 -0500

    getPokemonOptions functionality

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -13,6 +13,10 @@
 import PokemonPicture from '@/components/PokemonPicture'
 import PokemonOptions from '@/components/PokemonOptions'
 
+import getPokemonOptions from '@/helpers/getPokemonOptions'
+
+// console.log(getPokemonOptions())
+
 export default {
   components: {
     PokemonPicture,
